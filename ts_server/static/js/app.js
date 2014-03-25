697f65b468926cadf324a08fcbcf30f0aa81e998
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-03-25 13:45:59 -0400

    disable the front page for logged in users

--- ts_server/static/js/app.js
+++ ts_server/static/js/app.js
@@ -2,7 +2,7 @@
 * @Author: ziyuanliu
 * @Date:   2014-02-23 23:19:59
 * @Last Modified by:   ziyuanliu
-* @Last Modified time: 2014-03-25 13:44:33
+* @Last Modified time: 2014-03-25 13:45:56
 */
 $(document).ready(function() {
 
@@ -588,7 +588,7 @@ validate = function(response){
 	$('#plannertab').removeClass('hidden');
 	$('#front_create').prop('onclick',null);
 	$('#front_create').on('click',function(){$('#preferencestab').trigger('click');});
-	$('#preferencestab').trigger('click');
+	// $('#preferencestab').trigger('click');
 	load_preference();
 }
 
