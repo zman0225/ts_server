6df9be2b73192714655ce63ed97279e70ccb7a9c
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-03-29 21:52:55 -0400

    edited password

--- ts_server/email/send_weekly.py
+++ ts_server/email/send_weekly.py
@@ -3,7 +3,7 @@
 # @Author: ziyuanliu
 # @Date:   2014-03-27 18:23:27
 # @Last Modified by:   ziyuanliu
-# @Last Modified time: 2014-03-27 21:50:22
+# @Last Modified time: 2014-03-28 08:07:11
 # a script
 
 from ts_server.models.account import *
@@ -14,6 +14,7 @@ from ts_server.adapter.administration import connect_to_db
 
 import requests
 import logging
+import random
 # def send(self, kw):
 # 		for u in kw.keys():
 # 			data = loads(kw[u])
@@ -30,28 +31,40 @@ def template(recipes,username):
 	</p><h4>Cheers,</h4> <h3>TimeSavor</h3></body></html>"""%(username,s)
 
 
-def send_simple_message(email,name,msg, tag = None):
+def send_simple_message(email, msg, subject = None, tag = None):
 	return requests.post(
 		"https://api.mailgun.net/v2/mg.timesavorapp.com/messages",
 		auth=("api", "key-9y7c3fgcidcnqzopu-psjg0nq3wbg7h3"),
 		data={"from": "TimeSavor <chefsal@mg.timesavorapp.com>",
 			"to": [email],
-			"subject": "Hello, %s - your meals this week"%name,
+			"subject": subject,
 			"html": msg,
+			"o:tag":tag,
+			"o:campaign":"c7umr",
 			"o:tracking": True})
 
 if __name__ == '__main__':
 	connect_to_db("ts-server",'23.253.209.158',27017,'ts_server','a2e7rqej')
 	subscribed = 0
+
 	for acc in Account.objects:
 		if acc.subscribed:
+
+			email = acc.email
+			username = acc.username
+			rand = random.randint(0,2)
+			subjects = ["%s: Here's what you're cooking next week!"%username,"%s: Cooking next week will be SO easy"%username,"%s: Guess what? You're ready for dinner next week!"%username]
+			campaign = ["first","second","third"]
+
 			plan = acc.current_plan
 			plan = plan.menu_plan if plan is not None else None
-			if "solo" in acc.username.lower() and plan is not None and len(plan)>0:
-				recipe_list = [get_recipe_name_by_id(re) for re in plan]
-				temp = template(recipe_list,acc.username)
-				email = acc.email
-				username = acc.username
-				send_simple_message(email,username,temp)
-				logging.info("sent to %s"%username)
 
+			try:
+				if plan is not None:
+					recipe_list = [get_recipe_name_by_id(re) for re in plan]
+					temp = template(recipe_list,acc.username)
+					
+					send_simple_message(email,temp,subjects[rand],campaign[rand])
+					print "sent to %s"%username
+			except Exception as e:
+				print e
